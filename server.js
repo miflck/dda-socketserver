e062a21a0b1ec3464bd87484549af0f0e2a612e3
Michael Flückiger <contact@michaelflueckiger.ch>
2023-04-14 08:36:09 +0200

    emit

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ io.on("connection", (socket) => {
   socket.on("message", (data) => {
     console.log("message: " + data);
     socket.emit("message", data);
+    socket.broadcast.emit("message", data);
   });
 
   socket.on("disconnect", () => {
